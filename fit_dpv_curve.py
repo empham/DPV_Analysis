26718d88cdac7ed0eac5509bab898a8ab4717c6c
Emily Pham <emilyn3@hawaii.edu>
2025-02-07 08:49:37 -1000

    simplifications and rename

--- fit_dpv_curve.py
+++ fit_dpv_curve.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # from scipy.optimize import curve_fit
 from get_data_from_TSV import *
-from get_XY_from_plot import *
+from get_2_points_from_plot import *
 
 
 def fit_linear_drift(dpvDF):
@@ -26,8 +26,12 @@ def fit_linear_drift(dpvDF):
     to_float = np.vectorize(float)
     V_fwd = to_float(dpvDF[2:-1, 1])
     I_diff = to_float(dpvDF[2:-1, 2])
-    # Plot original data
-    get_XY_from_plot(V_fwd, I_diff)
+
+    # Get coordinates of linear regions
+    start1, end1 = get_2_points_from_plot(V_fwd, I_diff)
+    start2, end2 = get_2_points_from_plot(V_fwd, I_diff)
+    print(f"DEBUG: start1={start1} end1={end1}")
+    print(f"DEBUG: start2={start2} end2={end2}")
     return dpvDF  # @todo
 
 
---
+++ get_2_points_from_plot.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 15:59:35 2025
+
+@filename: get_2_points_from_plot.py
+@author: Emily
+"""
+
+import matplotlib.pyplot as plt
+
+
+def get_ordered_points(pointA, pointB):
+    """
+    Given two sets of  (X,Y) points returns them in the order start, end.
+
+    INPUT: 2 tuples (points)
+    DOES: orders them from smallest to largest value
+    OUTPUT: points in order
+    """
+    if pointA[0] < pointB[0]:
+        return (pointA, pointB)
+    else:
+        return (pointB, pointA)
+
+
+def get_2_points_from_plot(X_data, Y_data):
+    """
+    Given scatter plot data reutrns the two (X,Y) values clicked by user.
+
+    INPUT: X data and Y data as two numpy vectors
+    DOES: prompts user to click a point
+    OUTPUT: (X,Y) point clicked by user
+    """
+    # Create plot
+    plt.scatter(X_data, Y_data)
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Click on the plot to select start and end of target region")
+    plt.grid(True)
+
+    plt.show(block=False)
+    print("Click on the plot to select points...")
+
+    clicked_points = plt.ginput(2, show_clicks=True)
+    start, end = get_ordered_points(clicked_points[0], clicked_points[1])
+    # print(f"DEBUG: start={start} end={end}")
+
+    plt.close()  # Close the plot after selection
+
+    return (start, end)
