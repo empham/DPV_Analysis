e7fccab61911f6b776e6b1cd7d94fdb08f83ff42
Emily Pham <emilyn3@hawaii.edu>
2025-02-07 13:07:49 -1000

    started skeleton for linear fitting

--- fit_dpv_curve.py
+++ fit_dpv_curve.py
@@ -7,11 +7,17 @@ Created on Wed Feb  5 14:35:03 2025
 """
 
 import numpy as np
+
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-# from scipy.optimize import curve_fit
 from get_data_from_TSV import *
-from get_2_points_from_plot import *
+from fit_utils import *
+
+
+def linear_func(x, m, b):
+    """Return the mathematic equation of a line."""
+    return m*x + b
 
 
 def fit_linear_drift(dpvDF):
@@ -32,6 +38,21 @@ def fit_linear_drift(dpvDF):
     start2, end2 = get_2_points_from_plot(V_fwd, I_diff)
     print(f"DEBUG: start1={start1} end1={end1}")
     print(f"DEBUG: start2={start2} end2={end2}")
+    
+# =============================================================================
+#     # Extract linear regions
+#     x_lin =
+#     y_lin = 
+# 
+#     # Perform linear fit
+#     popt, pcov = curve_fit(linear_func, x_lin, y_lin)
+#     
+#     # Plot results
+#     x_fit = np.linspace(min(V_fwd), max(V_fwd), 100)
+#     plt.figure()
+#     y_fit = 
+# =============================================================================
+
     return dpvDF  # @todo
 
 
